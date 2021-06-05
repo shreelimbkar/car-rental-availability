2fc34086eabc9cd820bb0d4038744d5e832b89b5
shreelimbkar <limbkarshree@gmail.com>
2021-06-05 13:02:57 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React application to display car rental availability</h1>
+    </div>
+  );
+}
+
+export default App;
